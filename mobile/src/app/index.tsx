26210787038b50b93c13214961f082b787d51c68
leojkonko <leonardolinomeyer@gmail.com>
2024-07-26 22:24:29 -0300

    add tailwindcss to project

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,9 +1,12 @@
 import { Image, Text, View } from "react-native";
+import { NativeWindStyleSheet } from "nativewind";
 
+// Certifique-se de que os estilos NativeWind são carregados corretamente
+NativeWindStyleSheet.create({});
 export default function Index() {
   return (
     <View>
-      <Text>Hello world</Text>
+      <Text className="text-red-500">Hello world</Text>
     </View>
   );
 }
