509b1b89dc644d56686895dd26068df9b38b644e
leojkonko <leonardolinomeyer@gmail.com>
2024-08-11 09:40:35 -0300

    show trip details

--- mobile/src/app/trip/[id].tsx
+++ mobile/src/app/trip/[id].tsx
@@ -1,8 +1,17 @@
-import { ActivityIndicator, Text, View } from "react-native";
-import { useLocalSearchParams } from "expo-router";
-import { useState } from "react";
-import { TripDetails } from "@/server/trip-server";
+import {
+  ActivityIndicator,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { TripDetails, tripServer } from "@/server/trip-server";
 import Loader from "../loader";
+import { MapPin, Settings2 } from "lucide-react-native";
+import { colors } from "@/styles/color";
+import dayjs from "dayjs";
 
 type TripData = TripDetails & { when: string };
 
@@ -16,12 +25,40 @@ export default function Trip() {
   async function getTripDetails() {
     try {
       setIsLoadingTrip(true);
+
+      if (!tripId) {
+        return router.back();
+      }
+
+      const trip = await tripServer.getById(tripId);
+
+      const maxLengthDestination = 14;
+      const destination =
+        trip.destination.length > maxLengthDestination
+          ? trip.destination.slice(0, maxLengthDestination) + "..."
+          : trip.destination;
+
+      setTripDetails({
+        ...trip,
+        when: "",
+      });
     } catch (error) {
       console.log(error);
     } finally {
       setIsLoadingTrip(false);
     }
   }
+
+  const startDate = dayjs(tripDetails.starts_at);
+  const endDate = dayjs(tripDetails.ends_at);
+  // Formatar as datas
+  const formattedDate = `${startDate.format("DD")} de ${startDate.format(
+    "MMM"
+  )} a ${endDate.format("DD")} de ${endDate.format("MMM")}.`;
+
+  useEffect(() => {
+    getTripDetails();
+  }, []);
 
   if (isLoadingTrip) {
     return <Loader />;
@@ -30,9 +67,26 @@ export default function Trip() {
   return (
     <>
       <View className="flex-1 px-5 pt-16">
-        <View>
-          {/* <ActivityIndicator /> */}
-          <Text className="text-white">Trip</Text>
+        <View className="w-full bg-zinc-900 py-3 px-4 space-y-5 rounded-xl my-8 border border-zinc-800">
+          <View className="flex w-100 flex-row items-center space-x-2">
+            <MapPin color={colors.zinc[500]} size={20} />
+            <TextInput
+              className="text-zinc-100 flex-1 text-lg flex pb-1"
+              // placeholder={tripDetails.destination}
+              readOnly={true}
+            >
+              <Text className="text-zinc-400 text-lg truncate">
+                {tripDetails.destination}
+              </Text>
+            </TextInput>
+            <Text className="text-zinc-400 text-sm w-20">{formattedDate}</Text>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              className="w-9 h-9 bg-zinc-800 items-center justify-center flex rounded"
+            >
+              <Settings2 color={colors.zinc[500]} size={20} />
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </>
